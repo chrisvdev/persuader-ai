d5339c613c7a71563c91ff822bdc5be21b07e080
Christian Villegas <chrisvill.2312@gmail.com>
2023-02-04 23:53:40 -0300

    Fixed copy to clipboard

--- app/src/components/Response.jsx
+++ app/src/components/Response.jsx
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function Response({ response, i }) {
-  const [copied, setCopied] = useState(false);
-  const copiedEffect = () => {
-    setCopied(true);
+  const [copyStatus, setCopyStatus] = useState({
+    copied: false,
+    achievedCopy: false,
+  });
+  const copiedEffect = (success) => {
+    setCopyStatus({ copied: true, achievedCopy: success });
     setTimeout(() => {
-      setCopied(false);
+      setCopyStatus((state) => {
+        return { ...state, copied: false };
+      });
     }, 2000);
   };
 
+  const copier = () => {
+    navigator.clipboard
+      .writeText(response)
+      .then(() => {
+        copiedEffect(true);
+      })
+      .catch(() => {
+        copiedEffect(false);
+      });
+  };
+
   return (
-    <CopyToClipboard onCopy={copiedEffect} text={response}>
-      <p
-        title="Click this to copy on clipboard!"
-        className="flex py-2 px-4 my-1 mx-2 border rounded border-amber-600 hover:border-amber-400 hover:scale-105 hover:bg-indigo-700 active:scale-95 active:border-amber-200 active:bg-indigo-500 sm:text-xl transition-all"
-        key={`ai-response_${i}`}
-      >
-        {!copied ? response : "Copied to the clipboard!"}
-      </p>
-    </CopyToClipboard>
+    <p
+      onClick={copier}
+      title="Click this to copy on clipboard!"
+      className="flex py-2 px-4 my-1 mx-2 border rounded border-amber-600 hover:border-amber-400 hover:scale-105 hover:bg-indigo-700 active:scale-95 active:border-amber-200 active:bg-indigo-500 sm:text-xl transition-all"
+      key={`ai-response_${i}`}
+    >
+      {!copyStatus.copied
+        ? response
+        : copyStatus.achievedCopy
+        ? "Copied to the clipboard!"
+        : "Error copying to the clipboard 😭"}
+    </p>
   );
 }
-
 
 export default Response;
